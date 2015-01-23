06affa514c4765f2bc93e783c6c846166e57671c
rajatgrg96 <rajatgrg96@gmail.com>
2015-01-23 23:17:42 +0530

    ggggg

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+     inv_x <- NULL
+     set <- function(y) {
+         x <<- y
+         inv_x <<- NULL
+     }
+     get <- function() x
+     setinverse<- function(inverse) inv_x <<-inverse
+     getinverse <- function() inv_x
+     list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 
-}
+
 
 
 ## Write a short comment describing this function
